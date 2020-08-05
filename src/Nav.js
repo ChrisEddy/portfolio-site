a1284587e4bf79d9898b2bdf16e29fde47f0b1d6
Chris <chris.eddy.dev@gmail.com>
2020-08-05 12:52:24 -0700

    fix z-index on nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,25 @@
-import React,  { useState } from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'antd';
 import { MailOutlined, AppstoreOutlined } from '@ant-design/icons';
 
 const Nav = props => {
     const [currentTab, setCurrentTab] = useState('home')
     return (
-        <Menu className='Nav' onClick={setCurrentTab} selectedKeys={[currentTab]} mode="horizontal">
-            <Menu.Item key="mail" icon={<MailOutlined />}>
-                Navigation One
+        <div className='Nav'>
+            <Menu onClick={setCurrentTab} selectedKeys={[currentTab]} mode="horizontal">
+                <Menu.Item key="mail" icon={<MailOutlined />}>
+                    Navigation One
             </Menu.Item>
-            <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-                Navigation Two
+                <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+                    Navigation Two
             </Menu.Item>
-            <Menu.Item key="alipay">
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                    Navigation Four - Link
+                <Menu.Item key="alipay">
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                        Navigation Four - Link
                 </a>
-            </Menu.Item>
-        </Menu>
+                </Menu.Item>
+            </Menu>
+        </div>
     )
 }
 export default Nav;
